ae0a536a5381549af4da0559fd494da315231975
David Escala <descala@ingent.net>
2011-01-10 14:32:28 +0100

    empty models

--- application/test/unit/rates_test.rb
+++
@@ -1,9 +0,0 @@
-require File.dirname(__FILE__) + '/../test_helper'
-class RatesTest < Test::Unit::TestCase
-  fixtures :rates
-
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
-end
